d8fa2da89ebbff917b18b3541fea259cafbe6e37
FrancoDetarsio <detarsiolp@outlook.com>
2025-11-09 22:32:38 -0300

    big_update

--- Trabajo_Integrador_Programacion1.py
+++ Trabajo_Integrador_Programacion1.py
@@ -26,7 +26,7 @@ def verificar_inputs(opcion):
 
     while not(opcion.isdigit()):
         print("El valor ingresado no corresponde con un número entero")
-        opcion = input("Ingrese nuevamente el valor deseado: ")
+        opcion = input("Ingrese nuevamente el valor deseado.\n► ")
 
     if opcion.isdigit():
         opcion = int(opcion)
@@ -149,6 +149,227 @@ def filtro_rango_sup(lista, rango1, rango2):
     else:
         print("Los rangos ingresados son iguales\nIntente nuevamente con un rango mínimo y un rango máximo distintos.")
 
+def ordenar_por_nombre(lista, opcion):
+    """
+    Ordena la lista de países por nombre, ascendente o descendente.
+    
+    Args:
+        Datos: lista actualizada del archivo.csv y opcion elegida por el usuario
+
+    Returns:
+        si la opcion = '1' → lista ascendente por nombre
+        si la opcion = '2' → lista descendente por nombre
+
+    """
+    for i in range(len(lista) - 1):
+        for j in range(i + 1, len(lista)):
+            nombre_i = lista[i]["nombre"].lower()
+            nombre_j = lista[j]["nombre"].lower()
+
+            if opcion == '1':  # ascendente
+                if nombre_i > nombre_j:
+                    lista[i], lista[j] = lista[j], lista[i]
+            elif opcion == '2':  # descendente
+                if nombre_i < nombre_j:
+                    lista[i], lista[j] = lista[j], lista[i]
+
+
+    # Mostramos el resultado en pantalla
+    if opcion == '1':
+        print("\nPaíses ordenados por nombre ascendente:")
+        for pais in lista:
+            print(f"• País: {pais['nombre']} | Población: {pais['poblacion']} | Superficie: {pais['superficie']} km² |  Continente: {pais['continente']}")
+    elif opcion == '2':
+        print("\nPaíses ordenados por nombre descendente:")
+        for pais in lista:
+            print(f"• País: {pais['nombre']} | Población: {pais['poblacion']} | Superficie: {pais['superficie']} km² |  Continente: {pais['continente']}")
+    else:
+        print("\nPor favor, ingrese un número válido del menú para ordenar ascendente o descendentemente.")   
+
+def ordenar_por_poblacion(lista, opcion):
+    """
+    Ordena la lista de países por población, ascendente o descendente.
+    
+    Args:
+        Datos: lista actualizada del archivo.csv y opcion elegida por el usuario
+
+    Returns:
+        si la opcion = '1' → lista ascendente por población
+        si la opcion = '2' → lista descendente por población
+
+    """
+    for i in range(len(lista) - 1):
+        for j in range(i + 1, len(lista)):
+            # Convertimos los valores a enteros para comparar correctamente
+            poblacion_i = int(lista[i]["poblacion"])
+            poblacion_j = int(lista[j]["poblacion"])
+
+            if opcion == '1':  # ascendente
+                if poblacion_i > poblacion_j:
+                    lista[i], lista[j] = lista[j], lista[i]
+            elif opcion == '2':  # descendente
+                if poblacion_i < poblacion_j:
+                    lista[i], lista[j] = lista[j], lista[i]
+
+    # Mostramos el resultado ordenado
+    if opcion == '1':
+        print("\nPaíses ordenados por población:")
+        for pais in lista:
+            print(f"• País: {pais['nombre']} | Población: {pais['poblacion']} | Superficie: {pais['superficie']} km² | Continente: {pais['continente']}")  
+    elif opcion == '2':
+        print("\nPaíses ordenados por población:")
+        for pais in lista:
+            print(f"• País: {pais['nombre']} | Población: {pais['poblacion']} | Superficie: {pais['superficie']} km² | Continente: {pais['continente']}")  
+    else:
+        print("Por favor, ingrese un número válido del menú para ordenar ascendente o descendentemente.")
+
+def ordenar_por_superficie(lista, opcion):
+    """
+    Ordena la lista de países por Superficie, ascendente o descendente.
+    
+    Args:
+        Datos: lista actualizada del archivo.csv y opcion elegida por el usuario
+
+    Returns:
+        si la opcion = '1' → lista ascendente por superficie
+        si la opcion = '2' → lista descendente por superficie
+    """
+    for i in range(len(lista) - 1):
+        for j in range(i + 1, len(lista)):
+            # Convertimos los valores a enteros para comparar correctamente
+            superficie_i = int(lista[i]["superficie"])
+            superficie_j = int(lista[j]["superficie"])
+
+            if opcion == '1':  # ascendente
+                if superficie_i > superficie_j:
+                    lista[i], lista[j] = lista[j], lista[i]
+            elif opcion == '2':  # descendente
+                if superficie_i < superficie_j:
+                    lista[i], lista[j] = lista[j], lista[i]
+
+    # Mostramos el resultado ordenado
+    if opcion == '1':
+        print("\nPaíses ordenados por superficie:")
+        for pais in lista:
+            print(f"• País: {pais['nombre']} | Población: {pais['poblacion']} | Superficie: {pais['superficie']} km² | Continente: {pais['continente']}")  
+    elif opcion == '2':
+        print("\nPaíses ordenados por población:")
+        for pais in lista:
+            print(f"• País: {pais['nombre']} | Población: {pais['poblacion']} | Superficie: {pais['superficie']} km² | Continente: {pais['continente']}")  
+    else:
+        print("Por favor, ingrese un número válido del menú para ordenar ascendente o descendentemente.")
+
+def mayor_menor_pob(lista):
+    """
+
+    Búsca y muestra por pantalla los países de menor y mayor población
+
+    Args:
+        Dato: Lista
+    
+    Returns:
+        Print país con mayor y menor población
+
+    """
+
+    # Definimos variables con valores superiores a los presentes en la lista a las que les asignaremos que país mostrar
+    mayor = float("-inf")
+    menor = float("inf")
+
+    for i in lista:
+        poblacion = int(i["poblacion"]) # Pasamos el valor a entero para poder comparar
+
+        if poblacion < menor:
+            menor = poblacion # dejamos registrado cual fue el valor menor para la siguiente iteracion
+
+            # registramos en 4 variables los valores de la fila actual
+            men1, men2, men3, men4 = i["nombre"], i["poblacion"], i["superficie"], i["continente"]
+
+        if poblacion > mayor:
+            mayor = poblacion # dejamos registrado cual fue el valor mayor para la siguiente iteracion
+
+            # registramos en 4 variables los valores de la fila actual
+            may1, may2, may3, may4 = i["nombre"], i["poblacion"], i["superficie"], i["continente"]
+
+    # Mostramos resultados
+    print("Los países con Mayor y Menor Población son:\n")
+    print(f"• Menor → País: {men1} | Población: {men2} | Superficie: {men3}km² | Continente: {men4}\n")
+    print(f"• Mayor → País: {may1} | Población: {may2} | Superficie: {may3}km² | Continente: {may4}")
+
+def promedio_poblacion(lista):
+    """
+
+    Múestra el promedio de población de los países dentro del archivo.csv
+
+    Args:
+        Dato: lista actualizada del archivo.csv
+    
+    Returns:
+        promedio de población
+
+    """
+    # iniciamos variables que servirán de apoyo a la función
+    contador = 0
+    suma = 0
+
+    for i in lista:
+        contador += 1 # cuenta la cantidad de países en la lista para hacer promedio
+        poblacion = int(i["poblacion"]) # pasamos la poblacion a entero para luego sumar el total
+        suma += poblacion
+
+    # calculamos el promedio de poblacion
+    promedio = int(suma / contador) # int para obtener la parte entera
+
+    return promedio
+
+def promedio_superficie(lista):
+    """
+
+    Múestra el promedio de superficie de los países dentro del archivo.csv
+
+    Args:
+        Dato: lista actualizada del archivo.csv
+    
+    Returns:
+        promedio de superficie
+
+    """
+    # iniciamos variables que servirán de apoyo a la función
+    contador = 0
+    suma = 0
+
+    for i in lista:
+        contador += 1 # cuenta la cantidad de países en la lista para hacer promedio
+        superficie = int(i["superficie"]) # pasamos la superficie a entero para luego sumar el total
+        suma += superficie
+
+    # calculamos el promedio de superficie
+    promedio = int(suma / contador) # int para obtener la parte entera
+
+    return promedio
+
+def pais_por_continente(lista, continente):
+    """
+
+    Cuenta la cantidad de países que pertenecen a el continente que ingresa el usuario utilizando archivo.csv
+
+    Args:
+        Datos: lista actualizada del archivo.csv y continente ingresado por el usuario
+
+    Returns:
+        cantidad de continentes pertenecientes a dicho continente
+
+    """
+
+    contador = 0 # variable que contará los países pertenecientes
+
+    for i in lista:
+        # por cada coincidencia sumaremos un país
+        if i["continente"] == continente:
+            contador += 1
+
+    return contador
+
 # Aclaraciones
 # A lo largo del programa se usara "i" como iterador y marcador de indice
 # Se utilizará .strip() y .capitalize() para asegurarnos que las entradas tengan la misma escritura que la lista
@@ -168,20 +389,23 @@ while opcion_menu != '5':
     print("======== Gestor Informático de Países ========")
     print("|                                            |")
     print("|           1. Buscar País                   |")
-    print("|           2. Filtrar países                |")
+    print("|           2. Filtrar Países                |")
     print("|           3. Ordenar Países                |")
-    print("|           4. Mostrar estadísticas          |")
+    print("|           4. Mostrar Estadísticas          |")
     print("|           5. Salir                         |")
     print("|                                            |")
     print("==============================================\n")
 
-    opcion_menu = input("_ ").strip()
+    opcion_menu = input("► ").strip()
     print("")
 
     match opcion_menu:
         case '1':
-            pais = input("Ingrese el nombre del país que desea buscar: ").strip().capitalize()
+            print("================ Buscar País ================\n")
+
+            pais = input("Ingrese el nombre del país que desea buscar.\n► ").strip().title()
             lectura_csv(lista_paises) # obtenemos los datos desde la funcion
+            print("")
 
             bandera = False # usamos bandera para mostrar mensaje al usuario en caso de no encontrar el país ingresado
             for i in lista_paises:
@@ -193,32 +417,34 @@ while opcion_menu != '5':
                 print("El país ingresado no se encuentra en la lista")
 
         case '2':
+            print("=============== Filtrar Países ===============\n")
+
             print("Ingrese el número corresponidente al filtro que desea aplicar.")
-            opcion = input("1 - Continente\n2 - Rango de Población\n3 - Rango de Superficie\n_").strip()
-            opcion = verificar_inputs(opcion) # nos aseguramos que el iinput sea un numero entero
+            opcion = input("1 - Continente\n2 - Rango de Población\n3 - Rango de Superficie\n► ").strip()
+            opcion = verificar_inputs(opcion) # nos aseguramos que el input sea un numero entero
             lectura_csv(lista_paises)
             
             match opcion:
                 case 1:
-                    continente = input("Ingrese el nombre del continente que desea filtrar: ").strip().capitalize()
+                    continente = input("Ingrese el nombre del continente que desea filtrar.\n► ").strip().title()
                     filtro_continente(lista_paises, continente)
                     print("")
 
                 case 2:
-                    rango1 = input("Ingrese el primer valor correspondiente al rango mínimo: ")
-                    verificar_inputs(rango1)
+                    rango1 = input("Ingrese el primer valor correspondiente al rango mínimo.\n► ").strip()
+                    rango1 = verificar_inputs(rango1)
                     print("")
-                    rango2 = input("Ingrese el segundo valor correspondiente al rango máximo: ")
-                    verificar_inputs(rango2)
+                    rango2 = input("Ingrese el segundo valor correspondiente al rango máximo.\n► ").strip()
+                    rango2 = verificar_inputs(rango2)
                     
                     filtro_rango_pob(lista_paises, rango1, rango2)
 
                 case 3:
-                    rango1 = input("Ingrese el primer valor correspondiente al rango mínimo (km²): ")
-                    verificar_inputs(rango1)
+                    rango1 = input("Ingrese el primer valor correspondiente al rango mínimo (km²).\n► ").strip()
+                    rango1 = verificar_inputs(rango1)
                     print("")
-                    rango2 = input("Ingrese el segundo valor correspondiente al rango máximo (km²): ")
-                    verificar_inputs(rango2)
+                    rango2 = input("Ingrese el segundo valor correspondiente al rango máximo (km²).\n► ").strip()
+                    rango2 = verificar_inputs(rango2)
 
                     filtro_rango_sup(lista_paises, rango1, rango2)
 
@@ -226,13 +452,84 @@ while opcion_menu != '5':
                     print("El número ingresado no corresponde con las opciones\nIntente nuevamente.")
 
         case '3':
-            pass
+            print("=============== Ordenar Países ===============\n")
+
+            print("Ingrese el número corresponidente al ordenamiento que desea aplicar.")
+            opcion = input("1 - Por Nombre\n2 - Por Población\n3 - Por Superficie\n► ").strip()
+            opcion = verificar_inputs(opcion) # nos aseguramos que el input sea un numero entero
+            lectura_csv(lista_paises)
+            
+            match opcion:
+                case 1:
+                    print('Ingrese si desea ordenar por nombre de manera ascendente o descendente.')
+                    opcion = input("1 - Ascendente\n2 - Descendente\n► ")
+                    opcion = verificar_inputs(opcion)
+                    ordenar_por_nombre(lista_paises, str(opcion))
+                
+                case 2:
+                    print('Ingrese si desea ordenar por población de manera ascendente o descendente.')
+                    opcion = input("1 - Ascendente\n2 - Descendente\n► ")
+                    opcion = verificar_inputs(opcion) 
+                    ordenar_por_poblacion(lista_paises, str(opcion)) 
+                
+                case 3:
+                    print('Ingrese si desea ordenar por superficie de manera ascendente o descendente.')
+                    opcion = input("1 - Ascendente\n2 - Descendente\n► ")
+                    opcion = verificar_inputs(opcion) 
+                    ordenar_por_superficie(lista_paises, str(opcion))
+                
+                case _:
+                    print("El número ingresado no corresponde con las opciones\nIntente nuevamente.")
 
         case '4':
-            pass
+            print("============ Mostrar Estadísticas ============\n")
+
+            print("Ingrese el número corresponidente a la estadística que desea aplicar.")
+            print("1 - País con mayor y menor población\n2 - Promedio de población\n3 - Promedio de superficie\n4 - Cantidad de países por continente")
+            opcion = input("► ").strip()
+            opcion = verificar_inputs(opcion) # nos aseguramos que el input sea un numero entero
+            lectura_csv(lista_paises)
+
+            match opcion:
+                case 1:
+                    print("")
+
+                    # recurrimos a la funcón que imprimirá por pantalla la respuesta
+                    mayor_menor_pob(lista_paises)
+
+                case 2:
+                    print("")
+
+                    promedio = promedio_poblacion(lista_paises) # usamos la función que nos da el promedio
+                    print(f"El promedio de población de los países en la lista es: {promedio}")
+
+                case 3:
+                    print("")
+
+                    promedio = promedio_superficie(lista_paises) # usamos la función que nos da el promedio
+                    print(f"El promedio de superficie de los países en la lista es de: {promedio}km²")
+
+                case 4:
+                    print("")
+                    bandera = False
+                    continente = input("Ingrese el continente sobre el cual desea saber la cantidad de países que pertenecen a este.\n► ").strip().title()
+
+                    # verificamos que el continente pertenezca a uno en la lista
+                    for i in lista_paises:
+                        if i["continente"] == continente:
+                            bandera = True
+                        
+                    if bandera == True: # si el continente existe hará la bandera True y ejecuta la respuesta
+                        cantidad = pais_por_continente(lista_paises, continente) # tomamos la cantidad devuelta por la funcion
+                        print(f"El continente {continente} cuenta con {cantidad} Países en esta lista.")
+                    else:
+                        print("El continente ingresado no corresponde con uno dentro de la lista.")
+
+                case _:
+                    print("El número ingresado no corresponde con las opciones\nIntente nuevamente.")
 
         case '5':
-            print("================== Salir ==================")
+            print("=================== Salir ===================\n")
             print("¡Gracias por usar el Gestor Informático de Países!\nHasta Luego.")
 
         case _:
